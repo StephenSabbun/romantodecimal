ab8974f92e737ebf594ade540a09d6a946d2e9f2
StephenSabbun <83604169+StephenSabbun@users.noreply.github.com>
2022-09-02 16:10:55 +0800

    Add files via upload

---
+++ convert-reverse.js
@@ -0,0 +1,16 @@
+function convertToRoman(integerValue) {
+    let romanResult = '';
+    const decimalValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const romanValues = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    
+    for (let index = 0; index < decimalValues.length; index++) {
+        while (decimalValues[index] <= integerValue) {
+            romanResult += romanValues[index];
+            integerValue -= decimalValues[index];
+        }
+    }
+    console.log(romanResult);
+}
+const prompt = require("prompt-sync")({ sigint: true })
+let integer = prompt("Integer: ")
+convertToRoman(`${integer}`)
